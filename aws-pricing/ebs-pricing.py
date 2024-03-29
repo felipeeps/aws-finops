c8f032d9c799f2c83a0cbda65ed936aca7d87131
Felipe Pereira <felipeepereiras@gmail.com>
2024-03-29 15:05:26 +0000

    Add aws-pricing path

---
+++ aws-pricing/ebs-pricing.py
@@ -0,0 +1,12 @@
+import boto3
+
+# WIP - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/pricing.html
+
+def get_ebs_storage_price(region):
+    # WIP
+
+def main():
+    # WIP
+
+if __name__ == "__main__":
+    main()
