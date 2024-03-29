fdb34fa38cece678966f475e233782ea52f039f2
Felipe Pereira <felipeepereiras@gmail.com>
2024-03-29 15:26:25 +0000

    Add more infos to ELP

--- cost-optimization/elastic-ips.py
+++ cost-optimization/elastic-ips.py
@@ -8,7 +8,7 @@ def get_unused_elastic_ips(region):
 
     for address in response['Addresses']:
         if 'InstanceId' not in address:
-            unused_elastic_ips.append(address['PublicIp'])
+            unused_elastic_ips.append(address)
 
     return unused_elastic_ips
 
@@ -23,7 +23,17 @@ def main():
     if unused_elastic_ips:
         print("Unused Elastic IPs in the {} region: ".format(region))
         for ip in unused_elastic_ips:
-            print(" -", ip)
+            print(" - Elastic Public IP:", ip['PublicIp'])
+            print("   ", ip['AllocationId'])
+            print("   ", ip['NetworkInterfaceId'])
+            print("   ", ip['PrivateIpAddress'])
+            tags = ip.get('Tags', [])
+            if tags:
+                print("    TAGs:")
+                for tag in tags:
+                    print("     - {}: {}".format(tag['Key'], tag['Value']))
+            else:
+                print ("   No tags associated with this ELP")
     else:
         print("No unused Elastic IPs found in the {} region.".format(region))
 
