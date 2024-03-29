9e3eb6691fcb34a1d43b75a0239ef22e93ca291a
Felipe Pereira <felipeepereiras@gmail.com>
2024-03-29 15:18:35 +0000

    Add more infos to ec2 stoppeds

--- cost-optimization/ec2-stopped.py
+++ cost-optimization/ec2-stopped.py
@@ -9,7 +9,7 @@ def get_stopped_instances(region):
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
             if instance['State']['Name'] == 'stopped':
-                stopped_instances.append(instance['InstanceId'])
+                stopped_instances.append(instance)
 
     return stopped_instances
 
@@ -21,8 +21,16 @@ def main():
     stopped_instances = get_stopped_instances(region)
     if stopped_instances:
         print("Stopped instances {}: ".format(region))
-        for instance_id in stopped_instances:
-            print(" -", instance_id)
+        for instance in stopped_instances:
+            print(" - Instance ID:", instance['InstanceId'])
+            print("   ", instance['InstanceType'])
+            tags = instance.get('Tags', [])
+            if tags:
+                print("    TAGs:")
+                for tag in tags:
+                    print("     - {}: {}".format(tag['Key'], tag['Value']))
+            else:
+                print ("   No tags associated with this volume")
     else:
         print("Not found Stopped instances {}.".format(region))
 
