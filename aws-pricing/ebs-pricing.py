4167bbb8b5d1ed444bc305242376539295d2cb8c
Felipe Pereira <felipeepereiras@gmail.com>
2024-03-29 15:45:36 +0000

    Test pricing api boto3

--- aws-pricing/ebs-pricing.py
+++ aws-pricing/ebs-pricing.py
@@ -1,12 +1,20 @@
 import boto3
 
 # WIP - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/pricing.html
+# https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/eu-west-1/index.json
+# https://github.com/lyft/awspricing
+
 
 def get_ebs_storage_price(region):
     # WIP
+    pricing = boto3.client('pricing', region_name=region)
+    response = pricing.get_price_list_file_url()
+
+    print(response)
 
 def main():
     # WIP
+    get_ebs_storage_price('us-east-1')
 
 if __name__ == "__main__":
     main()
